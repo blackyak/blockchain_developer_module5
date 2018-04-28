342d220b74f3e1780574e5415e54834877c27124
Eenae <z-line@my.com>
2018-04-28 14:21:28 +0300

    initial version

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.18;
+
+
+contract HelloWorld {
+
+    constructor(string name) public {
+        m_name = name;
+    }
+
+    function hello() public view returns (string) {
+        return m_name;
+    }
+
+    string m_name;
+}
